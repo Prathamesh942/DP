fe10b7a1abef7d030bc5572e3b81982262ffa73a
Prathamesh khetre <87293621+Prathamesh942@users.noreply.github.com>
2024-09-28 02:53:20 +0000

    Min deletion insertion

---
+++ longest common subsequence/MinInsertionDeletion.cpp
@@ -0,0 +1,7 @@
+// Given two strings str1 and str2. The task is to remove or insert the minimum number of characters from/in str1 so as to transform it into str2. It could be possible that the same character needs to be removed/deleted from one point of str1 and inserted to some another point.
+
+// heap pea
+// lcs -> ea
+// delete h insert p
+
+// minimum deletion and insertions required = length(s1)+length(s2)-2*length(lcs);
